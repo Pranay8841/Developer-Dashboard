1062e080be790c2dcd50e0ae151d0f15501e696c
Pranay Bhandrkar <pranay.22110947@viit.ac.in>
2024-06-06 12:49:00 +0530

    All Initialization Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import mockData from './MockAPIData.json'
+import Dashboard from "./Components/Dashboard";
 
 function App() {
-  
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    setData(mockData);
+  }, []);
+
+  console.log(data)
+
   return (
-   <div>
-      Hello India
+   <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">Developer Activity Dashboard</h1>
+      {
+        data ?
+        <Dashboard data = {data} /> :
+        <p>Loading...</p>
+      }
    </div>
   );
 }
